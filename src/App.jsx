2b8073a7f82bbf8d9f754ec8fa2e44ac5905e0ad
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-05-10 16:29:11 +0200

    Import axios, return test values

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  [signedIn, setSignedIn] = useState()
+  const [petsList, setPetsList] = useState([]);
 
+  useEffect(() => {
+    axios.get('https://api-pets.adaptable.app/users')
+    .then(result => {
+      setPetsList(result.data);
+      console.log(result.data);
+
+    })
+    .catch(error => {
+      console.log("error", error);
+    
+
+  })}, []);
+
+return (
+  <>
+  <div>The dogs:</div>
+
+{petsList.map((characterObj, index) => {
   return (
-    <>
-      <div>Try to commit to orgnaizations repo</div>
-    </>
+      <div key={index}>
+      <p>Name: {characterObj.name}</p>
+      <p>Breed: {characterObj.password}</p>
+      </div>
+
   )
+})}
+</>
+);
 }
 
-export default App
+export default App;
