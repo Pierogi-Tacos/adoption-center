fb15be74aab0255f5dfa378b42cefbb916c81807
Suaste Cherizola <jsuaste86@gmail.com>
2024-05-13 17:57:24 +0200

    CRUD completegit add .git add .!

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,17 +1,150 @@
-export default function EditForm ({itemToEdit}) {
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const EditForm = ({itemToEdit}) => {
+
   
+  const [newPet, setNewPet] = useState({...itemToEdit});
+    function handleChange(e) {
+    let newObject = { ...newPet };
+    newObject[e.target.name] = e.target.value;
+    setNewPet(newObject);
+  }
 
-  console.log(itemToEdit)
-  return ( 
+  function handleImageUpload() {
+    console.log("logic to create the image");
+  }
 
-    <div className="edit-form">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`https://api-pets.adaptable.app/pets/${newPet.id}`, newPet)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  return (  
+    
+    <form onSubmit={handleSubmit} className="edit-form">
       <h1>Edit form</h1>
+      <label htmlFor="name">Pet's name</label>
+      <input
+        type="text"
+        name="name"
+        value={newPet.name}
+        onChange={handleChange}
+        placeholder={newPet.name}
+        /* required */
+      />
 
-    
-    
-    
-    </div>
+      <label htmlFor="breed">Pet's breed</label>
+      <input
+        type="text"
+        name="breed"
+        value={newPet.breed}
+        onChange={handleChange}
+        placeholder={newPet.breed}
+        /* required */
+      />
 
-  )
+      <label htmlFor="age">Pet's age</label>
+      <input
+        type="number"
+        name="age"
+        value={newPet.age}
+        onChange={handleChange}
+        placeholder={newPet.age}
+      />
 
-}
+      <label htmlFor="location">Location</label>
+      <input
+        type="text"
+        name="location"
+        value={newPet.location}
+        onChange={handleChange}
+        placeholder={newPet.location}
+        /* required */
+      />
+
+      <label htmlFor="vaccination">Vaccination</label>
+      <select
+        name="vaccination"
+        value={newPet.vaccination}
+        onChange={handleChange} /* required */
+      >
+        <option value={true}>Yes</option>
+        <option value={false}>No</option>
+      </select>
+
+      <label htmlFor="gender">Gender</label>
+      <select
+        name="gender"
+        value={newPet.gender}
+        onChange={handleChange} /* required */
+      >
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+
+      <label htmlFor="size">Size</label>
+      <select
+        name="size"
+        value={newPet.size}
+        onChange={handleChange} /* required */
+      >
+        <option value={"small"}>Small</option>
+        <option value={"medium"}>Medium</option>
+        <option value={"large"}>Large</option>
+      </select>
+
+      <label htmlFor="likes_kids">Likes kids?</label>
+      <select
+        name="likes_kids"
+        value={newPet.likes_kids}
+        onChange={handleChange} /*/* required */
+      >
+        <option value={true}>Yes</option>
+        <option value={false}>No</option>
+      </select>
+
+      <label htmlFor="description">Description</label>
+      <textarea
+        name="description"
+        value={newPet.description}
+        onChange={handleChange}
+        placeholder={newPet.description}
+        /* required */
+      />
+
+      <label htmlFor="owner">Owner</label>
+      <input
+        type="text"
+        name="owner"
+        value={newPet.owner}
+        onChange={handleChange}
+        placeholder={newPet.owner}
+      />
+
+      <label htmlFor="image">Image URL</label>
+      <input
+        type="url"
+        name="image"
+        value={newPet.image}
+        onChange={handleChange}
+        placeholder={newPet.image}
+      />
+
+      <label htmlFor="imageFile">Upload Image</label>
+
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+
+      <button type="submit">Save Changes</button>
+    </form>
+  );
+};
+export default EditForm;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, handleClick} from "react";
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect, handleClick } from "react";
+import { useNavigate } from "react-router-dom";
 import AddPetForm from "../components/AddPetForm";
 import axios from "axios";
 import EditForm from "../components/EditForm";
 
-export default function Admin ( {adminLogged}) {
+export default function Admin({ adminLogged }) {
   /* const navigate = useNavigate();
 
   if (!adminLogged) {
@@ -17,36 +17,15 @@ export default function Admin ( {adminLogged}) {
       ) 
   } */
 
-  // The real component starts here 
+  // The real component starts here
   const [petsList, setPetsList] = useState([]);
-  const [dogDetails, setDogDetails] = useState(false);
   const [displayEditForm, setDisplayEditForm] = useState(false);
-  const [indexElementToEdit, setIndexElementToEdit] = useState(null)
-  const [arrayToShow, setArrayToShow] = useState([])
-
-  function handleDetails(){
-
-  }
-  function handleEdit(){
-
-  }
-  function handleDelete(index){
-const newList = [...petsList];
-
-newList.splice(index, 1)
-
-    setPetsList(newList)
-    handleDelete()
-
-    // it needs to send information about the deleted item to the API
-
-
-  }
-  
-
+  const [indexElementToEdit, setIndexElementToEdit] = useState(null);
+  const [arrayToShow, setArrayToShow] = useState([]);
 
   useEffect(() => {
-    axios.get("https://api-pets.adaptable.app/pets")
+    axios
+      .get("https://api-pets.adaptable.app/pets")
       .then((result) => {
         setPetsList(result.data);
       })
@@ -55,65 +34,55 @@ newList.splice(index, 1)
       });
   }, []);
 
-  function handleDelete() {
+  function handleDelete(index, petId){
+    const newList = [...petsList];
+    newList.splice(index, 1)
+    setPetsList(newList)
+    axios.delete("https://api-pets.adaptable.app/pets/" + petId)
+      .then(response =>
+      console.log(response) )
+      .catch(error => console.log(error))
+    }
 
-    
-
-    
-  }
   function handleEdit(index) {
-    setIndexElementToEdit(index)
-    setDisplayEditForm(true)
+    setIndexElementToEdit(index);
+    setDisplayEditForm(true);
   }
 
   function handleDetails() {
-
-    //Delete element from the API; 
-    //Delete element from the Array; 
-    
+   
   }
 
   return (
- 
-   <div>
+    <div>
       <h1>Welcome Admin!</h1>
-      <div>
+      <div></div>
 
-      </div>
-        
       <AddPetForm/>
-      
-      {displayEditForm && 
-      <EditForm itemToEdit={petsList[indexElementToEdit]}/>
-      }
 
-    <button onClick={() => setDogDetails(prev => !prev)}>Show Details</button>
+      {displayEditForm && (
+        <EditForm itemToEdit={petsList[indexElementToEdit]} />
+      )}
 
       <div className="dogs-list">
-      {petsList.map((characterObj, index) => {
-        return (
-          <div key={index} className="dog-item">
-            <img src="https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg"/>
-            <p>{characterObj.name}</p>
-            <p>{characterObj.breed}</p>
-            <p>{characterObj.age} years</p>
-            <p>{characterObj.gender}</p>
+        {petsList.map((characterObj, index) => {
+          return (
+            <div key={index} className="dog-item">
+              <img src="https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg" />
+              <p>{characterObj.name}</p>
+              <p>{characterObj.breed}</p>
+              <p>{characterObj.age} years</p>
+              <p>{characterObj.gender}</p>
 
-            <div className="admin-buttons"> 
-              <button onClick={handleDetails}>Details</button>
-              <button onClick={handleEdit}>Edit</button>
-              <button onClick={() => handleDelete(index)}>Delete</button>
+              <div className="admin-buttons">
+                <button onClick={handleDetails}>Details</button>
+                <button onClick={ () => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDelete(index, characterObj.id)}>Delete</button>
+              </div>
             </div>
-          </div>
-
-        );
-      })}
+          );
+        })}
       </div>
-  
-      
-   </div>
-  )
-
+    </div>
+  );
 }
-
-
