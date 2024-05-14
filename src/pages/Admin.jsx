54f48a3fdb1d837dfe974fc75d1a88b6ccb0ef14
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-05-14 22:02:19 +0200

    Display dog cards and form

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -124,24 +124,27 @@ export default function Admin({ adminLogged }) {
         <SearchBar activateSearch={activateSearch}/>
         {arrayToShow.map((characterObj, index) => {
           return (
-            <div className="col-md-4">
+            <div key={characterObj.id || index} className="col-md-4">
             <div key={index} className="dog-item">
-            <div className="dog-item">
-                <div className="dog-photos">
-              <img src="https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg" />
+                <div className="dog-photos card p-2 m-2 mb-3 shadow">
+                  <img className="card-img-top rounded" src="https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg" />
+                  
+              
+              <h4 className="card-header font-weight-bold">{characterObj.name}</h4>
+              <div className="card-body">
+              <p className="card-subtitle mb-2 ">{characterObj.breed}</p>
+              <span className="card-subtitle mb-2 text-muted"><span>{characterObj.age} years, </span> 
+              <span>{characterObj.gender}</span></span>
               </div>
-              <p>{characterObj.name}</p>
-              <p>{characterObj.breed}</p>
-              <p>{characterObj.age} years</p>
-              <p>{characterObj.gender}</p>
-              </div>
+              
 
-              <div className="admin-buttons">
-                <button onClick={handleDetails}>Details</button>
-                <button onClick={() => handleEdit(index)}>Edit</button>
-                <button onClick={() => handleDelete(index, characterObj.id)}>
+              <div className="admin-buttons btn-group mx-auto" role="group">
+                <button type="button" className="btn btn-secondary border border-dark" onClick={handleDetails}>Details</button>
+                <button type="button" className="btn btn-secondary border border-dark" onClick={() => handleEdit(index)}>Edit</button>
+                <button type="button" className="btn btn-secondary border border-dark" onClick={() => handleDelete(index, characterObj.id)}>
                   Delete
                 </button>
+                </div>
               </div>
               </div>
             </div>
